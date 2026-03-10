302ea08ef80dc6e85738f380013e2bc943453b75
VerasDF <verasdf@gmail.com>
2026-03-10 14:46:19 -0300

    Adicionar formatação de texto para o botão de critério na interface

--- v3.0/motor.js
+++ v3.0/motor.js
@@ -757,7 +757,7 @@ const dados = {
         btnTemp.ariaLabel = `${strTexto}`;
         btnTemp.ariaPressed = 'false';
         btnTemp.className = 'campoCriterio';
-        btnTemp.innerHTML = `${strTexto}`;
+        btnTemp.innerHTML = `<nobr>${strTexto}</nobr>`;
         btnTemp.addEventListener('click', (e)=>{
             if(btnTemp.ariaPressed=="true"){btnTemp.ariaPressed="false"}else{btnTemp.ariaPressed="true"}
             filtrar.processarClickDoBotao(btnTemp);
